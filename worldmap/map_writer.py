f5f3299064feee878e33e58680a2f542db581fba
nvaldez <nolivaldeziii@gmail.com>
2020-05-31 17:14:28 +0800

    edited some asset

--- worldmap/map_writer.py
+++ worldmap/map_writer.py
@@ -3,10 +3,11 @@ import struct
 
 class MapWriter:
     STRUCT_FORMAT = '<HHII'
+    TEST_DIMENTION = 80
     def __init__(self):
         with open('map.bin', 'wb') as bin_out:
-            for x in range(0, 255):
-                for y in range(0, 255):
+            for x in range(0, MapWriter.TEST_DIMENTION):
+                for y in range(0, MapWriter.TEST_DIMENTION):
                     entry = struct.pack(MapWriter.STRUCT_FORMAT, 1, 0, x, y)
                     bin_out.write(entry)
                     bin_out.flush()
